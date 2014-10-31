609c60382e2932b93a9ef9e4aa822b5c915a072a
John Henry <john@iamjohnhenry.com>
2014-10-30 19:29:58 -0700

    Moved thanks.md to proper directory.

--- test/scripts/wscat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export URL=http://$HOST/$KEY?queue=$QUEUE\&binary=$BINARY\&type=$TYPE\&public=$PUBLIC\&full=$FULL\&peek=$PEEK\&subscribe=$SUBSCRIBE\&listen=$LISTEN
-echo $URL
-wscat -c $URL
